0e3da3426fae50fc162c03db590b58f704cf9e70
Ege Demirci <52569205+egedemirci@users.noreply.github.com>
2022-05-11 12:03:26 +0300

    Dark Mode Colors

---
+++ project_ace/lib/utilities/darkMode.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Welcome
+  static const Color welcomeScreenBackgroundColor = Color(0xFF003049);
+  // Login
+  static const Color loginFormBackgroundColor = Colors.white;
+  static const Color loginFormTextColor = Colors.grey; // TODO: Check this color
+  static const Color loginScreenBackgroundColor = Color(0xFF003049);
+  static const Color loginSignupButtonBackgroundColor = Color(0xFFFBBC05);
+  static const Color loginSignupButtonTextColor = Color(0xFF003049);
+  static const Color loginPageMetaGoogleOptionBackgroundColor =
+  Color(0xFFCDE8F4);
+  static const Color loginPageTextColor = Color(0xFFCDE8F4);
+  // Sign Up
+  static const Color signUpScreenBackgroundColor = Color(0xFF003049);
+  static const Color signUpTextColor = Color(0xFFCDE8F4);
+  static const Color signUpButtonBackgroundColor = Color(0xFFCDE8F4);
+  static const Color signUpButtonTextColor = Color(0xFFFB8500);
+  static const Color signUpFormBackgroundColor =
+      Colors.white;
+  static const Color signUpFormTextColor = Colors.grey;
+  // Shared Among Views
+  static const Color bottomNavigationBarFillColor = Color(0xFF003049);
+  static const Color bottomNavigationBarIconOutlineColor = Color(0xFFCDE8F4);
+  // Profile
+  static const Color profileIconColor = Color(0xFF003049);
+  static const Color profileSettingButtonFillColor =
+  Color(0xFF88A0B1); // TODO: Check this color
+  static const Color profileSettingsButtonTextColor =
+      Colors.white; // TODO: Check this color
+  static const Color profileScreenBackgroundColor = Color(0xFF003049);
+  static const Color profilePostViewButtonBackgroundColor = Color(0xFFF7FDFF);
+  static const Color profileScreenTextColor = Color(0xFFB9D3DF);
+  // Messages
+  // Posts
+  static const Color postTextColor = Color(0xFFB9D3DF);
+  static const Color userFullNameColor = Color(0xFFB9D3DF);
+  static const Color userNameColor = Color(0xFFB9D3DF);
+  // Create Post
+  static const Color mainAppPostTextColor = Color(0xFFD9D9D9);
+  static const Color mainAppSmallUsernameColor = Color(0xFFFBBC05);
+  static const Color bottomNavigationBarOutlineColor = Color(0xFFFB8500);
+  static const Color bottomNavigationBarBackgroundColor = Color(0xFF001E31);
+  static const Color messagesFromUserFillColor = Color(0xFF34A853);
+  static const Color messagesToUserFillColor = Color(0xFFCDE8F4);
+
+  static const Color metaGoogleConnectButtonColor = Color(0xFFCDE8F4);
+  static const Color deleteAccountButtonFillColor = Color(0xFF780000);
+  static const Color deactivateAccountButtonFillColor = Color(0xFFFB8500);
+}
