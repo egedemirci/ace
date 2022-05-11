49446664f38ca76a937b3db9a622ff00a11bcd5d
Efe Tüzün <efetuzun@sabanciuniv.edu>
2022-05-11 18:23:38 +0300

    Firestore Lecture Added to Main
    I have added the Firestore (uploading an image to the cloud) to the main as comments. Check it out!

--- project_ace/lib/main.dart
+++ project_ace/lib/main.dart
@@ -1,4 +1,6 @@
 // This file is connected to the Draft Branch
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +19,9 @@ import 'package:project_ace/utilities/bloc_observer.dart';
 import 'package:project_ace/utilities/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:project_ace/page_routes/homePage.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -179,6 +184,7 @@ class ErrorScreen extends StatelessWidget {
       );
     }
   }
+
 // Streams and Providers
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -199,6 +205,152 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+// Firestore
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyFirebaseApp());
+}
+
+class MyFirebaseApp extends StatefulWidget {
+  const MyFirebaseApp({Key? key}) : super(key: key);
+
+  @override
+  _MyFirebaseAppState createState() => _MyFirebaseAppState();
+}
+
+class _MyFirebaseAppState extends State<MyFirebaseApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Text(
+                    'No Firebase Connection: ${snapshot.error.toString()}'),
+              ),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const MaterialApp(
+            home: MyApp(),
+          );
+        }
+        return const MaterialApp(
+          home: Center(
+            child: Text('Connecting to Firebase'),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final ImagePicker _picker = ImagePicker();
+  XFile? _image;
+
+  Future pickImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = pickedFile;
+    });
+  }
+
+  Future uploadImageToFirebase(BuildContext context) async {
+    String fileName = basename(_image!.path);
+    Reference firebaseStorageRef =
+        FirebaseStorage.instance.ref().child('uploads/$fileName');
+    try {
+      await firebaseStorageRef.putFile(File(_image!.path));
+      print("Upload complete");
+      setState(() {
+        _image = null;
+      });
+    } on FirebaseException catch (e) {
+      print('ERROR: ${e.code} - ${e.message}');
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CS310 Firebase'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Center(child: Text('Connected to Firebase')),
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  Container(
+                    height: double.infinity,
+                    margin: const EdgeInsets.only(left: 30, right: 30, top: 10),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: _image != null
+                            ? Image.file(File(_image!.path))
+                            : TextButton(
+                                child: const Icon(
+                                  Icons.add_a_photo,
+                                  size: 150,
+                                ),
+                                onPressed: pickImage,
+                              )),
+                  )
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (_image != null)
+                  OutlinedButton(
+                    onPressed: () {
+                      uploadImageToFirebase(context);
+                    },
+                    child: const Text('Upload image'),
+                  ),
+                const SizedBox(
+                  width: 16,
+                ),
+                if (_image != null)
+                  OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        _image = null;
+                      });
+                    },
+                    child: const Text('Cancel'),
+                  ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
